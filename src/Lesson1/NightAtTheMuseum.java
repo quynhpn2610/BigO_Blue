d4835fff55603b68d841fd7ad8a8f1559e3afc5c
Quynh Nguyen <quynhpn2610@Quynhs-MacBook-Air.local>
2022-10-12 12:51:24 -0400

    add comments

--- src/Lesson1/NightAtTheMuseum.java
+++ src/Lesson1/NightAtTheMuseum.java
@@ -9,10 +9,12 @@ public class NightAtTheMuseum {
         char[] sArr = s.toCharArray();
         int ans = 0;
 
+        // Check difference from 'a' to first char of the string
         int diff = Math.abs('a'-sArr[0]);
-        if (diff > 13) ans += 26 - diff;
+        if (diff > 13) ans += 26 - diff; // if diff is larger than half of the circle, should go the other way round
         else ans += diff;
 
+        // Check from first char to the rest
         for (int i = 0; i < sArr.length-1; i++) {
             diff = Math.abs(sArr[i+1] - sArr[i]);
             if (diff > 13){
