ec049a1cca535224f9b63b5d7c488b14b8462249
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2022-12-20 09:05:41 -0800

    vitaly

--- src/Lesson1_DynamicArrays_String/Vitaly_StringComparison.java
+++ src/Lesson1_DynamicArrays_String/Vitaly_StringComparison.java
@@ -3,27 +3,27 @@ package Lesson1_DynamicArrays_String;
 import java.util.Scanner;
 
 public class Vitaly_StringComparison {
-    public static String compareString (String s1, String s2){
-        for (int i = s1.length()-1; i >= 0; i--) {
-            if (s1.charAt(i) == 'z'){
-                s1 = s1.substring(0, i) + 'a' + s1.substring(i+1);
+    public static String compareString (String s, String t){
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s.charAt(i) == 'z'){
+                s = s.substring(0, i) + 'a' + s.substring(i+1);
             }
             else {
-                s1 = s1.substring(0, i) + (char)(s1.charAt(i)+1) + s1.substring(i+1);
+                s = s.substring(0, i) + (char)(s.charAt(i)+1) + s.substring(i+1);
                 break;
             }
         }
 
-        if (s1.compareTo(s2) < 0){
-            return s1;
+        if (s.compareTo(t) < 0){
+            return s;
         }
         else return "No such string";
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
-        System.out.println(compareString(s1, s2));
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        System.out.println(compareString(s, t));
     }
 }
