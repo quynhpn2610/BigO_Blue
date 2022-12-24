a5e2faaffa36788929e1b773aa0523905d7d3fac
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2022-12-24 14:39:48 -0800

    constant range

--- src/Lesson2_AlgorithmComplexity/ConstantRange.java
+++ src/Lesson2_AlgorithmComplexity/ConstantRange.java
@@ -1,38 +1,48 @@
 package Lesson2_AlgorithmComplexity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class ConstantRange{
-    public static int findMaxLength(List<Integer> list){
-        int prev = -1;
-        int current = list.get(0);
-        int next;
-        int prevCount = 0;
-        int currentCount = 1;
+    public static int solution(List<Integer> list){
+        int maxCanGo = 1;
+        int firstPtr = 0;
+        int secondPtr = 1;
 
-        // longest constant range length
-        int longest = 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(list.get(firstPtr), 1);
 
-        for (int i = 1; i < list.size(); i++) {
-            next = list.get(i);
-            if (next == current){
-                currentCount ++;
-            } else if (next == prev) {
-                prevCount += currentCount;
-                prev = current;
-                current = next;
-                currentCount = 1;
-            } else {
-                longest = Math.max(longest, currentCount + prevCount);
-                prev = current;
-                prevCount = currentCount;
-                current = next;
-                currentCount = 1;
+        for (firstPtr = 0; firstPtr < list.size(); firstPtr++){
+            while (secondPtr < list.size()){
+                if (map.containsKey(list.get(secondPtr))){
+                    map.put(list.get(secondPtr), map.get(list.get(secondPtr))+1);
+                }
+                else{
+                    map.put(list.get(secondPtr), 1);
+                }
+
+                if (map.size() == 3){
+                    map.remove(list.get(secondPtr));
+                    break;
+                }
+
+                secondPtr++;
+            }
+
+            if (secondPtr - firstPtr > maxCanGo){
+                maxCanGo = secondPtr - firstPtr;
+            }
+
+            if (map.get(list.get(firstPtr)) == 1){
+                map.remove(list.get(firstPtr));
+            }
+            else{
+                map.put(list.get(firstPtr), map.get(list.get(firstPtr))-1);
             }
         }
-        return longest;
+        return maxCanGo;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -41,7 +51,7 @@ public class ConstantRange{
         for (int i = 0; i < n; i++) {
             list.add(Integer.parseInt(sc.next()));
         }
-        System.out.println(findMaxLength(list));
+        System.out.println(solution(list));
         sc.close();
     }
 }
